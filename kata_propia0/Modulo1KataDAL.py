6067224019acb0b846e1b15ef89184b20d24c043
Daniel-AlbertL <99633842+Daniel-AlbertL@users.noreply.github.com>
2022-02-20 13:09:43 -0600

    Actualización

---
+++ kata_propia0/Modulo1KataDAL.py
@@ -0,0 +1,52 @@
+##Progrma.py 
+suma = 1 + 2 
+print(suma)
+
+## Print()
+print('Hola desde la consola')
+
+## Variable s
+sum = 1 + 2 
+product = sum * 2
+print(product)
+
+##Tipos de datos ##
+planetas_en_el_sistema_solar = 8 
+distancia_a_alfa_centauri = 4.367 
+puede_despegar = True
+transbordador_que_aterrizo_en_la_luna = "Apollo 11" 
+## type##
+planetas_en_el_sistema_solar = 8 
+type(distancia_a_alfa_centauri) 
+puede_despegar = True
+transbordador_que_aterrizo_en_la_luna = "Apollo 11" 
+
+## Operadores 
+left_side = 10
+right_side = 5
+left_side / right_side 
+
+## Fecha 
+from datetime import date 
+date.today()
+print(date.today())
+
+## string lectura de datos 
+print("Today's date is: " + str(date.today()))
+
+## introduccion de datos Input 
+##print("Bienvenido al programa de bienvenida")
+##name = input("Introduzca su nombre ")
+##print("Saludos: " + name)
+
+## numeros 
+##print("Calculadora")
+##first_number = input("Primer número: ")
+##second_number = input("Segundo número: ")
+##print(first_number + second_number)
+
+## impresion 
+print("Calculadora")
+first_number = input("Primer número: ")
+second_number = input("Segundo número: ")
+print(int(first_number) + int(second_number))
