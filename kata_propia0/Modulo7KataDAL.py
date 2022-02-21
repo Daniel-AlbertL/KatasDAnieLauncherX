ebfe5e24e1c85a6352423dfee2a3fb96b1dca124
Daniel-AlbertL <99633842+Daniel-AlbertL@users.noreply.github.com>
2022-02-21 11:42:19 -0600

    upload 7

---
+++ kata_propia0/Modulo7KataDAL.py
@@ -0,0 +1,10 @@
+## kata ejercicio 1 
+new_planet = " "
+planets = []
+
+while new_planet.lower() != "done": 
+    if new_planet: 
+        planets.append(new_planet)
+        new_plane = input ("Enter para crear un Nuevo planeta ")
+
+
